3c280b3972be7f7f8ca93e417205866a3dd268ef
vrkgandla <vrkgandla@gmail.com>
2025-02-27 15:33:57 +0100

    Added ApplyJob and view modified

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -22,14 +22,14 @@ import {
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+import {useNavigate} from 'react-router-dom'
+import * as Constants from "../constants"
 
 const companyName = "Quest-Tek Solutions"
 const callsToAction = [
 ]
-
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-
   return (
     <header className="bg-white">
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
@@ -54,7 +54,8 @@ export default function Header() {
                    <a href="/" className="text-sm/6 font-semibold text-gray-900">
                     Jobs
                   </a>
-                  <a href="/company" className="text-sm/6 font-semibold text-gray-900">
+                  <a
+                  href={Constants.COMPANY} className="text-sm/6 font-semibold text-gray-900">
                     Company
                   </a>
                 </PopoverGroup>
@@ -89,7 +90,7 @@ export default function Header() {
                   Jobs
                 </a>
                 <a
-                  href="/company"
+                  href={Constants.COMPANY}
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
                   Company
